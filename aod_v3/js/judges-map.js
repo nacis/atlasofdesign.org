8ed6a775bcd4c51386b775719c550ae8f42c6615
mapsam <matthews.sam@gmail.com>
2016-05-14 15:37:49 -0700

    remove wordpress theme

--- aod_v3/js/judges-map.js
+++
@@ -1,5 +0,0 @@
-var map = L.mapbox.map('judge-map', 'omnitarian.hg4448jo', {minZoom:2, maxZoom:5}).setView([40, -74.50], 2);
-		
-map.featureLayer.on('mouseover', function(e) {e.layer.openPopup();});
-
-map.featureLayer.on('mouseout', function(e) {e.layer.closePopup();});
--- aod_v3/searchform.php
+++
@@ -1,6 +0,0 @@
-<form role="search" method="get" id="searchform" class="searchform" action="http://atlasofdesign.org">
-	<div>
-		<input type="text" value="" name="s" id="s" placehoder="Search..."/>
-		<input type="submit" id="searchsubmit" value="Search" />
-	</div>
-</form>
